a7eab5e8361ced2e1fd95ba39559bdea68c03905
Izzat Jamili <gih.nih@gmail.com>
2025-06-22 19:07:46 +0800

    Finish.

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,11 @@ class MyAppState extends ChangeNotifier {
     }
     notifyListeners();
   }
+
+  void removeFavorite(WordPair pair){
+      favorites.remove(pair);
+      notifyListeners();
+  }
 }
 
 class MyHomePage extends StatefulWidget {
@@ -182,24 +187,42 @@ class BigCard extends StatelessWidget {
 class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     var appState = context.watch<MyAppState>();
 
     if (appState.favorites.isEmpty) {
       return Center(child: Text('No favorites yet'));
     }
 
-    return ListView(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
-          padding: const EdgeInsets.all(29),
-          child: Text('You have ${appState.favorites.length} favorites'),
+          padding: const EdgeInsets.all(30),
+          child: Text('You have ${appState.favorites.length} favorites:'),
         ),
-
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
+        Expanded(
+          // Make better use of wide windows with a grid
+          child: GridView(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 400,
+              childAspectRatio: 400 / 80,
+            ),
+            children: [
+              for (var pair in appState.favorites)
+                ListTile(
+                  leading: IconButton(
+                    onPressed: () {
+                      appState.removeFavorite(pair);
+                    },
+                    icon: Icon(Icons.delete_outline, semanticLabel: 'Delete'),
+                    color: theme.colorScheme.primary,
+                  ),
+                  title: Text(pair.asLowerCase,semanticsLabel: pair.asPascalCase,),
+                ),
+            ],
           ),
+        ),
       ],
     );
   }
